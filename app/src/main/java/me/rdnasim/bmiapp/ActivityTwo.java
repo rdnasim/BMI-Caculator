4dd6e6610871549fcf768df527479cd0f546a76e
rdnasim <rdislamnasim78@gmail.com>
2017-10-07 00:54:03 +0600

    Initial commit

---
+++ app/src/main/java/me/rdnasim/bmiapp/ActivityTwo.java
@@ -0,0 +1,61 @@
+package me.rdnasim.bmiapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ActivityTwo extends AppCompatActivity {
+
+    EditText weightText;
+    EditText heightText;
+    Button calculateButton;
+    TextView yourBIM;
+    TextView yourResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+    }
+
+    public void CalculateBMI(View view) {
+
+        double weight = 0;
+        double height = 0;
+        double bmi = 0;
+        String msg = "";
+
+        weightText = (EditText) findViewById(R.id.weight_kg);
+        heightText = (EditText) findViewById(R.id.height_m);
+
+        calculateButton = (Button) findViewById(R.id.calculate_bim_BTN);
+
+        yourBIM = (TextView) findViewById(R.id.your_bim);
+        yourResult = (TextView) findViewById(R.id.your_result);
+
+        weight = Double.parseDouble(weightText.getText().toString());
+        height = Double.parseDouble(heightText.getText().toString());
+
+        bmi = height * height;
+        bmi = weight / bmi;
+
+        yourBIM.setText(String.valueOf(bmi));
+
+        if (bmi < 18.5) {
+            msg = "Under Weight";
+        } else if (bmi > 18.5 && bmi < 25) {
+            msg = "Normal";
+        } else if (bmi > 25) {
+            msg = "Over Weight";
+        }
+
+        yourResult.setText(msg);
+
+    }
+}
---
+++ app/src/main/java/me/rdnasim/bmiapp/MainActivity.java
@@ -0,0 +1,22 @@
+package me.rdnasim.bmiapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void CalculateNow(View view) {
+
+        Intent intent = new Intent(this, ActivityTwo.class);
+        startActivity(intent);
+
+    }
+}
