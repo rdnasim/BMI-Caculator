f2c90bf0a5198ebf5e05240d1795bd1ec0a088a7
rdnasim <rdislamnasim78@gmail.com>
2017-10-07 11:30:49 +0600

    updated

--- app/src/main/java/me/rdnasim/bmiapp/ActivityTwo.java
+++ app/src/main/java/me/rdnasim/bmiapp/ActivityTwo.java
@@ -48,11 +48,14 @@ public class ActivityTwo extends AppCompatActivity {
         yourBIM.setText(String.valueOf(bmi));
 
         if (bmi < 18.5) {
-            msg = "Under Weight";
-        } else if (bmi > 18.5 && bmi < 25) {
-            msg = "Normal";
-        } else if (bmi > 25) {
-            msg = "Over Weight";
+            msg = "UnderWeight";
+        } else if (bmi > 18.5 && bmi < 24.9) {
+            msg = "Normal Weight";
+        } else if (bmi > 25 && bmi < 29.9) {
+            msg = "OverWeight";
+        }
+        else if (bmi>30){
+            msg = "Obesity";
         }
 
         yourResult.setText(msg);
